ce366358305033c1c1a23f76e39aba46f9fa6593
旧事 <545049148@qq.com>
2019-01-13 15:58:35 +0800

    add README.md update 基本完成所有功能

--- getClassTable/index.js
+++ getClassTable/index.js
@@ -1,73 +1,138 @@
+/**
+ * 获取课表的函数
+ *  通过传入 term 学期 获得指定学期的课表
+ *  （此处还没考虑教务处没有课表的情况）
+ * 
+ * @param string uid
+ * @param string pwd
+ * @param string term
+ * 
+ * @return dict(Object)
+ *      classArray      list
+ *      classWeekDay    list
+ *      returnStatus    bool
+ *      errorMessage    string
+ */
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
-var request = require('request');
-// 开启 request 的 cookies记录
-// var request = request.defaults({jar: true})
-var j = request.jar()
-var request = request.defaults({
-  jar: j
+var rp = require('request-promise');
+
+// 开启 request-promise 的 cookies记录
+const api_url = 'http://39.107.243.115';
+var cookiejar = rp.jar();
+// cookiejar.setCookie(cookie, api_url); //请求api_url的所有cookies
+rp = rp.defaults({
+  jar: cookiejar
 })
+
 cloud.init()
 
 // 云函数入口函数
 exports.main = async(event, context) => {
-  const wxContext = cloud.getWXContext()
-
-  var username = event.uid;
+  // 返回数据
+  let returnMessage = {
+    returnStatus = true
+  }
+  // 传入的各个数据
+  let username = event.uid; //typeof -- string
+  let password = event.pwd; //typeof --> string
+  let term = event.term; //typeof --> string
+  // 数据库
   const db = cloud.database()
-  var dblog = null
-  var ifHavaTheUser = false;
-  await db.collection('kb_users').where({
-      uid: username
+  // 查询数据库中是否有这个学期的课表
+  let ifHaveThisTerm = false;
+  var resultData
+  await db.collection('kb_classtables').where({
+      uid: username,
+      term: term
     }).get()
     .then(result => {
-      dblog = result.data
-      var resultData = result.data;
+      resultData = result.data;
+      console.log(resultData)
       if (resultData.length != 0) {
-        ifHavaTheUser = true
+        ifHaveThisTerm = true
       }
     })
-  console.log('dblog:', dblog)
+  // 没有本节课则 请求 api 获取
+  let classMessage = {}
+  if (!ifHaveThisTerm) {
+    // 登录 服务器
+    await rp({
+      uri: api_url + '/wc/CqClassBox/lei',
+      method: 'POST',
+      formData: {
+        pw: '0005f2d9c91740c4af7114db2346004c'
+      },
+      jar: cookiejar
+    })
+    // 请求 term 指定学期的课表
+    await rp({
+      url: api_url + '/wc/CqClassBox/getClassTable',
+      method: 'POST',
+      formData: {
+        username: username,
+        password: username,
+        term: term
+      },
+      jar: cookiejar
+    }).then(body => {
+      // console.log('\n\n\nbody', body)
+      classMessage = JSON.parse(body)
+    }).catch(error => {
+      classMessage.messageStatus = false
+      classMessage.errorMessage = 'request api error'
+    })
 
-  var userMessage = 'null';
-  const api_url = 'http://39.107.243.115';
+    // 如果数据请求成功 则写入数据库
+    if (classMessage.messageStatus) {
+      // 将课表信息添加进 kb_classtables 中
+      await db.collection('kb_classtables').add({
+        data: {
+          term: term,
+          uid: username,
+          termClassTables: classMessage.classMessage
+        }
+      })
+      // 返回
+      let classArray = []     // 作为课表用
+      let classWeekDay = {    // 作为每日课程用
+        Sunday: [],
+        Monday: [],
+        Tuesday: [],
+        Wednesday: [],
+        Thursday: [],
+        Friday: [],
+        Saturday: []
+      }
+      const Weeks = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday','Sunday']
+      for (let course of classMessage.classMessage) {
+        let courseObject = {}
+        courseObject.courseName = course.courseName
+        courseObject.roomName = course.roomName
+        courseObject.weekDay = course.weekDay
+        courseObject.courseTime = course.courseTime
+        
+        classArray.push(courseObject)
 
-  await request({
-    url: api_url + '/wc/CqClassBox/lei',
-    method: 'POST',
-    formData: {
-      pw: '0005f2d9c91740c4af7114db2346004c'
+        courseObject.teacherName = course.teacherName
+        courseObject.vaildWeek = course.vaildWeeks.indexOf('1') + '-' + course.vaildWeeks.lastIndexOf('1')
+        
+        classWeekDay[Weeks[course.weekDay]].push(courseObject)
+      }
+      // 对每日课程进行排序
+      for(let day of Weeks){
+        classWeekDay[day].sort((a,b)=>{
+          return a.courseTime - b.courseTime
+        })
+      }
+      // 返回
+      returnMessage.classArray = classArray
+      returnMessage.classWeekDay = classWeekDay
+      return returnMessage
+    } else {
+      returnMessage.returnStatus = false
+      returnMessage.errorMessage = classMessage.errorMessage
+      return returnMessage
     }
-  }, function(error, response, body) {
-    // 登录成功返回
-    userMessage = body;
-    // userMessage = JSON.parse(body)
-    console.log('body', body)
-  })
-
-  await request({
-    url: api_url + '/wechart/CqClassBox/login',
-    method: 'POST',
-    formData: {
-      username: '201503306',
-      password: '201503306'
-    }
-  }, function(error, response, body) {
-    console.log('\n\n\n', body)
-    // var info = JSON.parse(body)
-    var info = ''
-    info = body
-    console.log('\ninfo', info)
-    userMessage = info
-  })
-
-  return {
-    userMessage,
-    ifHavaTheUser,
-    dblog,
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
   }
 }
